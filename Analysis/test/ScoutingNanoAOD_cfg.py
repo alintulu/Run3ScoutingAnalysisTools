bdf4d780adf35da1f123ab39d50cbdd5225bc4a3
alintulu <adelina.eleonora.lintuluoto@cern.ch>
2022-04-20 11:34:58 +0200

    update ScoutingNanoAOD_cfg

--- Analysis/test/ScoutingNanoAOD_cfg.py
+++ Analysis/test/ScoutingNanoAOD_cfg.py
@@ -5,73 +5,20 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 params = VarParsing('analysis')
 
 params.register(
-    'isQCD',
-    False,
-    VarParsing.multiplicity.singleton,VarParsing.varType.bool,
-    'Flag to indicate whether the sample is QCD or not'
-)
-
-params.register(
-    'isMC',
-    True,
-    VarParsing.multiplicity.singleton,VarParsing.varType.bool,
-    'Flag to indicate whether the sample is simulation or data'
-)
-
-params.register(
-    'useWeights',
-    False,
-    VarParsing.multiplicity.singleton,VarParsing.varType.bool,
-    'Flag to indicate whether or not to use the events weights from a Monte Carlo generator'
-)
-
-params.register(
-    'filterTrigger',
-    False,
-    VarParsing.multiplicity.singleton,VarParsing.varType.bool,
-    'Flag to indicate whether or not to ask the event to fire a trigger used in the analysis'
-)
-
-params.register(
-    'filterMuons',
-    False,
-    VarParsing.multiplicity.singleton,VarParsing.varType.bool,
-    'Flag to indicate whether or not to ask the event to contain at least two muons'
-)
-
-params.register(
-    'reducedInfo',
-    False,
-    VarParsing.multiplicity.singleton,VarParsing.varType.bool,
-    'Flag to indicate whether or not to store just the reduced information'
-)
-
-params.register(
-    'trigProcess',
-    'HLT',
-    VarParsing.multiplicity.singleton,VarParsing.varType.string,
-    'Process name for the HLT paths'
-)
-
-params.register(
-    'GlobalTagData',
-    '<not available>',
-    VarParsing.multiplicity.singleton,VarParsing.varType.string,
-    'Process name for the HLT paths'
-)
-
-params.register(
     'GlobalTagMC',
-    '112X_mcRun3_2021_realistic_v16',
+    '122X_mcRun3_2021_realistic_v5',
     VarParsing.multiplicity.singleton,VarParsing.varType.string,
     'Process name for the HLT paths'
 )
 
-params.register(
-    'xsec',
-    0.001,
-    VarParsing.multiplicity.singleton,VarParsing.varType.float,
-    'Cross-section for a Monte Carlo Sample'
+params.maxEvents = -1
+params.outputFile = "nano.root"
+
+params.register('inputDataset',
+    '',
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.string,
+    "Input dataset"
 )
 
 # Define the process
@@ -82,7 +29,7 @@ params.parseArguments()
 
 # Message Logger settings
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.MessageLogger.destinations = ['cout', 'cerr']
+#process.MessageLogger.destinations = ['cout', 'cerr']
 process.MessageLogger.cerr.FwkReport.reportEvery = 5
 
 # Set the process options -- Display summary at the end, enable unscheduled execution
@@ -113,24 +60,12 @@ process.load('PhysicsTools.PatAlgos.producersLayer1.patCandidates_cff')
 
 # Load the global tag
 from Configuration.AlCa.GlobalTag import GlobalTag
-if params.isMC :
-    process.GlobalTag.globaltag = params.GlobalTagMC
-else :
-    process.GlobalTag.globaltag = params.GlobalTagData
+process.GlobalTag.globaltag = params.GlobalTagMC
 
 # Define the services needed for the treemaker
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string(params.outputFile)
 )
-
-# Tree for the generator weights
-'''
-process.gentree = cms.EDAnalyzer("LHEWeightsTreeMaker",
-    lheInfo = cms.InputTag("externalLHEProducer"),
-    genInfo = cms.InputTag("generator"),
-    useLHEWeights = cms.bool(params.useWeights)
-)
-'''
 
 #from DarkPhotonAnalysis.DimuonAnalysis2018.TriggerPaths_cfi import getL1Conf
 L1Info = ['L1_DoubleMu4p5er2p0_SQ_OS_Mass_Min7', 'L1_DoubleMu_12_5','L1_DoubleMu_15_7','L1_TripleMu_5_3_3','L1_TripleMu_5_5_3','L1_QuadMu0','L1_DoubleMu0er1p5_SQ_OS_dR_Max1p4','L1_DoubleMu4p5er2p0_SQ_OS_Mass7to18','L1_DoubleMu4_SQ_OS_dR_Max1p2','L1_SingleMu22','L1_DoubleMu0er1p4_SQ_OS_dR_Max1p4','L1_DoubleMu4p5_SQ_OS_dR_Max1p2','L1_DoubleMu4p5_SQ_OS','L1_DoubleMu0er1p5_SQ_dR_Max1p4','L1_DoubleMu0er2p0_SQ_dR_Max1p4','L1_DoubleMu0_SQ']
@@ -142,7 +77,7 @@ process.genParticlesMerged = cms.EDProducer("MergedGenParticleProducer",
 # Make tree
 process.mmtree = cms.EDAnalyzer('ScoutingNanoAOD',
 
-    	triggerresults   = cms.InputTag("TriggerResults", "", params.trigProcess),
+    	triggerresults   = cms.InputTag("TriggerResults", "", "HLT"),
         doL1 = cms.bool(False),
         triggerConfiguration = cms.PSet(
     		hltResults            = cms.InputTag('TriggerResults','','HLT'),
@@ -167,7 +102,7 @@ process.mmtree = cms.EDAnalyzer('ScoutingNanoAOD',
         genpart          = cms.InputTag("prunedGenParticles"),
         # NEW
         slimjet          = cms.InputTag("slimmedJets"),
-        isQCD            = cms.bool( params.isQCD )
+        isQCD            = cms.bool( '/QCD_' in params.inputDataset )
         # genParticles = cms.InputTag("genParticlesMerged")
     	#pileupinfo       = cms.InputTag("addPileupInfo"),
     	#geneventinfo     = cms.InputTag("generator"),
