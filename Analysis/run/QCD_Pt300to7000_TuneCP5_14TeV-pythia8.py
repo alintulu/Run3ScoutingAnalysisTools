c520c8ee71bb086b95988dade2915dca73309e76
alintulu <adelina.eleonora.lintuluoto@cern.ch>
2022-07-28 11:01:18 +0200

    add crab files

--- Analysis/run/QCD_Pt300to7000_TuneCP5_14TeV-pythia8.py
+++ Analysis/run/QCD_Pt300to7000_TuneCP5_14TeV-pythia8.py
@@ -4,22 +4,22 @@ config.section_('General')
 config.General.transferLogs = False
 config.General.transferOutputs = True
 config.General.workArea = 'crab_projects'
-config.General.requestName = 'BulkGraviton_hh_GF_HH_14TeV_TuneCP5_pythia8'
+config.General.requestName = 'QCD_Pt300to7000_TuneCP5_14TeV-pythia8'
 config.section_('JobType')
 config.JobType.numCores = 1
 config.JobType.sendExternalFolder = True
-config.JobType.pyCfgParams = ['outputFile=nano.root', 'inputDataset=BulkGraviton_hh_GF_HH_14TeV_TuneCP5_pythia8']
+config.JobType.pyCfgParams = ['outputFile=nano.root', 'inputDataset=/QCD_Pt300to7000_TuneCP5_14TeV-pythia8']
 config.JobType.pluginName = 'Analysis'
 config.JobType.allowUndistributedCMSSW = True
 config.JobType.psetName = '../test/ScoutingNanoAOD_cfg.py'
 config.JobType.maxMemoryMB = 2000
 config.section_('Data')
-config.Data.userInputFiles = open('files_BulkGraviton.txt').readlines()
-config.Data.outputPrimaryDataset = 'BulkGraviton_hh_GF_HH_14TeV_TuneCP5_pythia8'
-config.Data.outputDatasetTag = 'BulkGraviton_hh_GF_HH_14TeV_TuneCP5_pythia8'
+config.Data.outputPrimaryDataset = 'QCD_Pt300to7000_TuneCP5_14TeV-pythia8'
+config.Data.outputDatasetTag = 'QCD_Pt300to7000_TuneCP5_14TeV-pythia8'
+config.Data.userInputFiles = open('files_QCD.txt').readlines()
 config.Data.publication = False
-config.Data.unitsPerJob = 10
-config.Data.inputDBS = 'phys03'
+config.Data.unitsPerJob = 5
+config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.allowNonValidInputDataset = True
 config.Data.outLFNDirBase = '/store/group/ml/Tagging4ScoutingHackathon/Adelina/DeepNtuples/12_3_0/ScoutingAK8-v00/Ntuples'
